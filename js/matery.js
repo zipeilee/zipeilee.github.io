3df9f2dfb18f3e77a5b4e105feceb1c21ea81026
zipeilee <lishin922@outlook.com>
2020-08-12 23:56:35 +0800

    Site updated: 2020-08-12 23:56:34

--- js/matery.js
+++ js/matery.js
@@ -177,7 +177,21 @@ window.onscroll = function() {
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("headNav").style.top = "0";
   } else {
-    document.getElementById("headNav").style.top = "-100px";
+    document.getElementById("headNav").style.top = "-60px";
   }
   prevScrollpos = currentScrollPos;
-}
+} 
+
+/*if ($('.smart-scroll').length > 0) { // check if element exists
+    var last_scroll_top = 0;
+    $(window).on('scroll', function() {
+        scroll_top = $(this).scrollTop();
+        if(scroll_top < last_scroll_top) {
+            $('.smart-scroll').removeClass('scrolled-down').addClass('scrolled-up');
+        }
+        else {
+            $('.smart-scroll').removeClass('scrolled-up').addClass('scrolled-down');
+        }
+        last_scroll_top = scroll_top;
+    });
+}*/
